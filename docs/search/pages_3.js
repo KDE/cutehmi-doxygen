6cb210a1f8aed138a18de852e939c2692e51cbf5
CuteBOT <michpolicht@gmail.com>
2018-07-20 18:36:32 +0200

    Autogenerate documentation.

--- docs/search/pages_3.js
+++ docs/search/pages_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['list_20of_20modules',['List of modules',['../modules_list.html',1,'']]],
-  ['list_20of_20qml_20modules',['List of QML modules',['../qml_list.html',1,'']]],
+  ['list_20of_20qml_20extensions',['List of QML extensions',['../qml_list.html',1,'']]],
   ['list_20of_20tools',['List of tools',['../tools_list.html',1,'']]]
 ];
