12dfbf835efc8aa9e515498ca4194b821f47b69c
CuteBOT <michpolicht@gmail.com>
2018-07-28 03:25:21 +0200

    Autogenerate documentation.

--- docs/recipes/search/pages_2.js
+++ docs/recipes/search/pages_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['libgit2',['libgit2',['../md_i686-w64-mingw32_libgit2_README.html',1,'']]],
   ['libmodbus',['Libmodbus',['../md_i686-w64-mingw32_libmodbus_README.html',1,'']]],
   ['libssh2',['LIBSSH2',['../md_i686-w64-mingw32_libssh2_README.html',1,'']]],
   ['libssh',['Libssh',['../md_i686-w64-mingw32_libssh_README.html',1,'']]]
