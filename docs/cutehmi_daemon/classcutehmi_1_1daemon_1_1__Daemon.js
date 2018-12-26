3955acd534ac152bb33bd2e817f2585a4cd50cc7
CuteBOT <michpolicht@gmail.com>
2018-12-26 03:15:46 +0100

    Autogenerate documentation.

--- docs/cutehmi_daemon/classcutehmi_1_1daemon_1_1__Daemon.js
+++ docs/cutehmi_daemon/classcutehmi_1_1daemon_1_1__Daemon.js
@@ -1,5 +1,8 @@
 var classcutehmi_1_1daemon_1_1__Daemon =
 [
+    [ "_Daemon", "classcutehmi_1_1daemon_1_1__Daemon.html#ab61c101e429b3d705b5f70e526cfa813", null ],
+    [ "destroyPidFile", "classcutehmi_1_1daemon_1_1__Daemon.html#aace2de425b680f26112d77cdef6cba62", null ],
+    [ "initializePidFile", "classcutehmi_1_1daemon_1_1__Daemon.html#ad7a16193f4061ce153aa34668ab5a66d", null ],
     [ "initializeSignalHandling", "classcutehmi_1_1daemon_1_1__Daemon.html#a282639efb55c7cb89373cef004430a77", null ],
     [ "terminateRequested", "classcutehmi_1_1daemon_1_1__Daemon.html#a0ea274671d26af8690aae1530b913eb4", null ]
 ];
--- docs/cutehmi_daemon/search/variables_e.js
+++ docs/cutehmi_daemon/search/variables_e.js
@@ -3,6 +3,7 @@ var searchData=
   ['patternoptions',['PatternOptions',['http://doc.qt.io/qt-5/qregularexpression.html#PatternOption-enum',1,'QRegularExpression']]],
   ['pausemodes',['PauseModes',['http://doc.qt.io/qt-5/qabstractsocket.html#PauseMode-enum',1,'QAbstractSocket']]],
   ['permissions',['Permissions',['http://doc.qt.io/qt-5/qfiledevice.html#Permission-enum',1,'QFileDevice']]],
+  ['pidfile',['pidfile',['../structcutehmi_1_1daemon_1_1CoreData_1_1Options.html#a80ce1b4392bc564b6f0e35110b4565ed',1,'cutehmi::daemon::CoreData::Options']]],
   ['pointer',['pointer',['http://doc.qt.io/qt-5/qjsonarray.html#pointer-typedef',1,'QJsonArray::pointer()'],['http://doc.qt.io/qt-5/qfuture-const-iterator.html#pointer-typedef',1,'QFuture::const_iterator::pointer()'],['http://doc.qt.io/qt-5/qlinkedlist.html#pointer-typedef',1,'QLinkedList::pointer()'],['http://doc.qt.io/qt-5/qlist.html#pointer-typedef',1,'QList::pointer()'],['http://doc.qt.io/qt-5/qset.html#pointer-typedef',1,'QSet::pointer()'],['http://doc.qt.io/qt-5/qstring.html#pointer-typedef',1,'QString::pointer()'],['http://doc.qt.io/qt-5/qvarlengtharray.html#pointer-typedef',1,'QVarLengthArray::pointer()'],['http://doc.qt.io/qt-5/qvector.html#pointer-typedef',1,'QVector::pointer()']]],
   ['policyflags',['PolicyFlags',['http://doc.qt.io/qt-5/qhstspolicy.html#PolicyFlag-enum',1,'QHstsPolicy']]],
   ['processeventsflags',['ProcessEventsFlags',['http://doc.qt.io/qt-5/qeventloop.html#ProcessEventsFlag-enum',1,'QEventLoop']]],
